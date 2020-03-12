38df4de29a624d2548634e55cba48137f8f87468
Lareta <lareta064@gmail.com>
2020-03-12 22:36:53 +0300

    секция документы

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    const icon1 = new Vivus('icon1', {		
-		duration: 300
-    });
-    const icon2 = new Vivus('icon2', {		
-		duration: 200
-    });
-    const icon3 = new Vivus('icon3', {		
-		duration: 300
-	});
+  const icon1 = new Vivus('icon1', {
+    duration: 300
+  });
+  const icon2 = new Vivus('icon2', {
+    duration: 200
+  });
+  const icon3 = new Vivus('icon3', {
+    duration: 200
+  });
 })

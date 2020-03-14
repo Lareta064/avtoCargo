6ee47d85282c4bd9fcd51090e33d9178e0d7e266
Lareta <lareta064@gmail.com>
2020-03-14 20:15:27 +0300

    страница перевозок

--- src/js/main.js
+++ src/js/main.js
@@ -1,142 +1,154 @@
 $(document).ready(function () {
 
-  /*выбор селекта в форме*/
-  const selectBlock = document.querySelectorAll('.select-block')
+	/*выбор селекта в форме*/
+	const selectBlock = document.querySelectorAll('.select-block')
 
-  for (let i = 0; i < selectBlock.length; i++) {
+	for (let i = 0; i < selectBlock.length; i++) {
 
-    selectBlock[i].addEventListener('click', function (e) {
+		selectBlock[i].addEventListener('click', function (e) {
 
-      const outInput = selectBlock[i].querySelector('.filters-form-input')
-      const itemOptions = selectBlock[i].querySelectorAll('.select-option')
-      let optionsDrop = selectBlock[i].querySelector('.select-block-drop')
-      let arrowOpenDrop = selectBlock[i].querySelector('.input-arrow')
+			const outInput = selectBlock[i].querySelector('.filters-form-input')
+			const itemOptions = selectBlock[i].querySelectorAll('.select-option')
+			let optionsDrop = selectBlock[i].querySelector('.select-block-drop')
+			let arrowOpenDrop = selectBlock[i].querySelector('.input-arrow')
 
-      // console.log(e.target)
-      if (e.target == outInput || e.target == arrowOpenDrop) {
-        e.stopPropagation()
+			// console.log(e.target)
+			if (e.target == outInput || e.target == arrowOpenDrop) {
+				e.stopPropagation()
 
-        for (let j = 0; j < selectBlock.length; j++) {
-          if (j != i) {
-            selectBlock[j].querySelector('.select-block-drop').classList.add('hide')
-            selectBlock[j].querySelector('.input-arrow').classList.remove('rotate')
-          }
-        }
+				for (let j = 0; j < selectBlock.length; j++) {
+					if (j != i) {
+						selectBlock[j].querySelector('.select-block-drop').classList.add('hide')
+						selectBlock[j].querySelector('.input-arrow').classList.remove('rotate')
+					}
+				}
 
-        if (optionsDrop.classList.contains('hide')) {
+				if (optionsDrop.classList.contains('hide')) {
 
-          arrowOpenDrop.classList.add('rotate')
-          optionsDrop.classList.remove('hide')
+					arrowOpenDrop.classList.add('rotate')
+					optionsDrop.classList.remove('hide')
 
-        } else {
+				} else {
 
-          optionsDrop.classList.add('hide')
-          arrowOpenDrop.classList.remove('rotate')
-        }
+					optionsDrop.classList.add('hide')
+					arrowOpenDrop.classList.remove('rotate')
+				}
 
-      }
+			}
 
-      const itemOptionsArray = Array.from(itemOptions)
+			const itemOptionsArray = Array.from(itemOptions)
 
-      for (let i = 0; i < itemOptionsArray.length; i++) {
+			for (let i = 0; i < itemOptionsArray.length; i++) {
 
-        if (e.target == itemOptionsArray[i]) {
-          e.stopPropagation()
+				if (e.target == itemOptionsArray[i]) {
+					e.stopPropagation()
 
-          const selectValue = itemOptionsArray[i].textContent
-          outInput.value = selectValue
-          optionsDrop.classList.add('hide')
-          arrowOpenDrop.classList.remove('rotate')
-        }
-      }
-    })
-  }
+					const selectValue = itemOptionsArray[i].textContent
+					outInput.value = selectValue
+					optionsDrop.classList.add('hide')
+					arrowOpenDrop.classList.remove('rotate')
+				}
+			}
+		})
+	}
 
-  /*показать скрытую часть формы Рассчитать стоимость*/
-  const showFormBtn = document.querySelectorAll('.show-fields');
-  const formPart = document.querySelector('#form-part');
+	/*показать скрытую часть формы Рассчитать стоимость*/
+	const showFormBtn = document.querySelectorAll('.show-fields');
+	const formPart = document.querySelector('#form-part');
 
-  for (let i = 0; i < showFormBtn.length; i++) {
-    showFormBtn[i].addEventListener('click', function () {
-      for (item of showFormBtn) {
-        item.classList.add('hide');
-      }
-      formPart.classList.remove('hide');
+	for (let i = 0; i < showFormBtn.length; i++) {
+		showFormBtn[i].addEventListener('click', function () {
+			for (item of showFormBtn) {
+				item.classList.add('hide');
+			}
+			formPart.classList.remove('hide');
 
-    });
-  }
+		});
+	}
 
-  /* при ресайзе экарна */
+	/* при ресайзе экарна */
 
-  window.addEventListener('resize', function () {});
+	window.addEventListener('resize', function () { });
 
-  /* Показать скрытые Завершенные Проекты страница О Компании*/
-  const completProjectsWrapper = document.querySelector('.project-cards');
-  const completProjectCard = completProjectsWrapper.querySelectorAll('.card-item');
-  const btnMoreCards = document.querySelector('#moreProjects');
+	/* Показать скрытые Завершенные Проекты страница О Компании*/
+	// const completProjectsWrapper = document.querySelector('.project-cards');
+	const completProjectCard = document.querySelectorAll('.project-cards .card-item');
+	const btnMoreCards = document.querySelector('#moreProjects');
 
-  if (btnMoreCards) {
-    btnMoreCards.addEventListener('click', function () {
+	if (btnMoreCards) {
+		btnMoreCards.addEventListener('click', function () {
 
-      this.classList.add('hide');
-      for (let card of completProjectCard) {
+			this.classList.add('hide');
+			for (let card of completProjectCard) {
 
-        card.classList.remove('hide');
-      }
-    })
-  }
-  /* Галерея работ */
-  //magnifyk
-  $(".card-item").magnificPopup({
-    delegate: 'a',
-    type: 'image',
+				card.classList.remove('hide');
+			}
+		});
+	}
 
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-    }
-  });
-  $('.partners-slider').owlCarousel({
-    items: 1,
-    dots: false,
-    loop: true,
-    autoplaySpeed: 800,
-    autoplay: true,
-    nav: true,
-    navSpeed: 500,
-    navText: ['<span class="slider-arrow slider-arrow-left">	&larr;</span>', '<span class="slider-arrow slider-arrow-right">&rarr;</span>'],
 
-    responsive: {
+	/* Галерея работ */
+	//magnifyk
+	$(".card-item").magnificPopup({
+		delegate: 'a',
+		type: 'image',
 
-      580: {
-        items: 2
-      },
-      992: {
-        items: 3
-      },
-      1200: {
-        items: 4
-      }
-    }
-  })
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+	$('.partners-slider').owlCarousel({
+		items: 1,
+		dots: false,
+		loop: true,
+		autoplaySpeed: 800,
+		autoplay: true,
+		nav: true,
+		navSpeed: 500,
+		navText: ['<span class="slider-arrow slider-arrow-left">	&larr;</span>', '<span class="slider-arrow slider-arrow-right">&rarr;</span>'],
 
-  //   $('.header-slider ').slick({
-  //     slidesToShow: 3,
-  //     slidesToScroll: 1,
-  //     centerMode: true,
-  //     variableWidth: true,
-  //     arrows: false,
-  //     dots: true,
-  //     infinite: true,
-  //     autoplay: true,
-  //     autoplaySpeed: 900,
+		responsive: {
 
-  //     responsive: [{
-  //         breakpoint: 1023,
-  //         settings: {
-  //             // dots: false
-  //         }
-  //     }]
-  // });
+			580: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	})
+
+	/*  Toggle Tranportation-types */
+	const transportTypes = document.querySelectorAll('.transportation-types-item');
+	const transportTypesInfo = document.querySelectorAll('.transportation-types-description');
+
+	if (transportTypes) {
+
+		for (let i = 0; i < transportTypes.length; i++) {
+			transportTypes[i].addEventListener('click', function () {
+
+				let thisData = this.getAttribute('data-card');
+
+				for (let j = 0; j < transportTypesInfo.length; j++) {
+
+					transportTypesInfo[j].classList.remove('show');
+					const textData = transportTypesInfo[j].getAttribute('data-info');
+
+					if (textData == thisData) {
+						transportTypesInfo[i].classList.add('show');
+					}
+				}
+
+			})
+		}
+
+	}
+
+
+
 })
